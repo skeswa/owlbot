8f3b54ec60dfe17938710fffd455a54aedbcbe40
Ubuntu <skeswa@tudevnet.tudevnet.b4.internal.cloudapp.net>
2015-02-01 12:41:00 -0500

    ...

--- twitter.go
+++ twitter.go
@@ -52,7 +52,7 @@ func NewTwitterConnector() (error, *TwitterConnector) {
 func (tc *TwitterConnector) listenForTweets() error {
 	// Create parameters for the request
 	params := url.Values{}
-	params.Set("track", "#owlhacks,#owlhacks2015,hackathons,#hackru")
+	params.Set("track", "#owlhacks,#owlhacks2015,hackathon,hackathons,#hackru")
 	// Get dat stream
 	stream, err := tc.api.PublicStreamFilter(params)
 	if err != nil {
